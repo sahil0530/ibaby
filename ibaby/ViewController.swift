4d5d3e7001b709f027854babcee2224b9263c9b0
Sahil Gupta <sahil@Sahils-MacBook-Pro.local>
2020-06-07 19:15:13 +0530

    initial xcode project creation

---
+++ ibaby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ibaby
+//
+//  Created by Sahil Gupta on 07/06/20.
+//  Copyright © 2020 Sahil Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
